32934ee35cd3647db914f80ddeda47a1281e904b
Ivan Martinez <jesulim@gmail.com>
2022-09-28 13:21:53 -0400

    add link pagelayout

--- components/PageLayout.jsx
+++ components/PageLayout.jsx
@@ -1,4 +1,5 @@
 import Head from "next/head"
+import Link from "next/link"
 
 export default function PageLayout({ children, title = 'NewsApp' }) {
   return (<>
@@ -7,13 +8,20 @@ export default function PageLayout({ children, title = 'NewsApp' }) {
       <meta name="description" content="newsapp - the best app to read news" />
       <link rel="icon" href="/favicon.ico" />
     </Head>
-    <header>🗞 newsapi</header>
+    <header>
+      <Link href='/'>🗞 newsapi</Link>
+      <div>
+        <Link href='/about'>About</Link>
+      </div>
+    </header>
     <main>
       {children}
     </main>
     <style jsx>{`
      header {
-       padding: 20px
+      display: flex;
+      justify-content: space-between;
+      padding: 20px;
      }
    `}</style>
   </>
